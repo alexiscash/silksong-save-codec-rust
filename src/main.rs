71422401d06ef0e298b45bf9ac253c57e9963dbe
Alexis Rangel <alexiscash97@rocketmail.com>
2026-02-17 01:28:29 -0600

    lowkey not working but thats ok

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use aes::cipher::BlockEncryptMut;
 use base64::{engine::general_purpose, Engine as _};
 use aes::Aes256;
 use ecb::Decryptor;
+use ecb::Encryptor;
 use block_padding::Pkcs7;
 use ecb::cipher::{BlockDecryptMut, KeyInit};
 
@@ -35,18 +37,60 @@ fn decrypt(payload: Vec<u8>) -> Result<serde_json::Value, Box<dyn std::error::Er
     Ok(json)
 }
 
-fn save_json(obj: serde_json::Value, path: &str) -> Result<(), Box<dyn std::error::Error>>{
-    let pretty = serde_json::to_string_pretty(&obj)?;
-    std::fs::write(path, &pretty)?;
-    Ok(())
+fn encrypt(json_obj: serde_json::Value) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let mut json_bytes = serde_json::to_string(&json_obj)?.into_bytes();
+
+    let cipher = Encryptor::<Aes256>::new_from_slice(AES_KEY_STRING.as_bytes())
+        .map_err(|e: aes::cipher::InvalidLength| format!("Invalid key length: {:?}",e))?;
+
+    let block_size = 16;
+    let msg_size = json_bytes.len();
+    let padded_len = ((msg_size / block_size) + 1) * block_size;
+
+    json_bytes.resize(padded_len, 0u8);
+
+    let encrypted = cipher.encrypt_padded_mut::<Pkcs7>(&mut json_bytes, msg_size)
+        .map_err(|e| format!("Padding error: {:?}", e))?;
+
+    let b64_bytes = general_purpose::STANDARD.encode(encrypted).into_bytes();
+
+    let mut out = Vec::<u8>::new();
+
+    let mut n = b64_bytes.len();
+    loop {
+        let byte = n & 0x7f;
+        n >>= 7;
+        if n > 0 {
+            out.push(byte as u8 | 0x80);
+        } else {
+            out.push(byte as u8);
+            break;
+        }
+    }
+
+    let mut final_bytes = Vec::<u8>::new();
+
+    final_bytes.extend_from_slice(&CSHARP_HEADER);
+    final_bytes.append(&mut out);
+    final_bytes.extend_from_slice(&b64_bytes);
+
+    Ok(final_bytes)
 }
+
+// fn save_json(obj: serde_json::Value, path: &str) -> Result<(), Box<dyn std::error::Error>>{
+//     let pretty = serde_json::to_string_pretty(&obj)?;
+//     std::fs::write(path, &pretty)?;
+//     Ok(())
+// }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let raw = std::fs::read("saves/user2.dat")?;
-
     let json = decrypt(raw)?;
+    let encrypted = encrypt(json.clone())?;
+    let decrypted = decrypt(encrypted)?;
+    assert_eq!(json, decrypted);
 
-    save_json(json, "saves/user2.json")?;
+    // let json_obj = serde_json::from_reader(rdr)
     
 
     Ok(())
